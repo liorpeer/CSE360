c37846e5438b9a21f084ec5c344e130443051658
Timer180 <tinpham1997@gmail.com>
2018-03-14 16:37:49 -0700

    Browse

--- Project/TextFileAnalyzer.java
+++ Project/TextFileAnalyzer.java
@@ -21,16 +21,19 @@ public	class	TextFileAnalyzer	{
         this.avgCharLine = 0;
         this.leftJustify = leftJustify;
         AnalyzeFile();
+
         writeOutputFile();
     }
     // upload file and analyze it
     private	void AnalyzeFile() throws IOException {
         int	totalChar	=	0;
+
         File	file	=	new	File(this.pathToTextFile);
         BufferedReader	br	=	new	BufferedReader(new	FileReader(file));
-        String	line	=	null;
-
+        String	line;
         while	((line	=	br.readLine())	!=	null)	{
+
+
             lineNumber	++;
 			char []letters = line.toCharArray();
 
@@ -43,8 +46,7 @@ public	class	TextFileAnalyzer	{
 
 				for(int i = 0; i < line.length();i++)
 				{
-						for(i = 0; i < line.length();i++)
-						{
+
 
 							//Checks if there's a char and a space in that order, if the last char at the last index is not a space then it counts as a word as well
 							//32 represents a space
@@ -54,12 +56,13 @@ public	class	TextFileAnalyzer	{
 									else if(i == line.length() - 1 && letters[i] != 32)
 											wordNumber++;
 							totalChar++;
-						}
+
            		}
 			}
 
 
         }
+
         avgWordPerLine	=	wordNumber	/	lineNumber;
         avgCharLine	=	totalChar / lineNumber;
         br.close();
