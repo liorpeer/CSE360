986a17d26ced7e1fa577125e093c2f22731feda7
Luke Mains <lcmains@asu.edu>
2018-03-25 21:44:29 -0700

    Added components.
    The components are just empty buttons right now. There is no
    functionality.

--- Project Final/src/Main.java
+++ Project Final/src/Main.java
@@ -31,42 +31,56 @@ import java.io.IOException;
 
 public class Main {
     //Global Variables
-    static int wordsProcessed, lines, blanksRemoved;
+    static int wordsProcessed, lines, blanksRemoved, spacesAdded;
     static double averageWPL;
     static double averageLL;
     static TextFileAnalyzer textFileAnalyzer;
 	static String input, output;
 	
     //Layouts
-    static FlowLayout flow = new FlowLayout(FlowLayout.CENTER, 50, 20);
+    static FlowLayout flow_right = new FlowLayout(FlowLayout.RIGHT, 40, 20);
+    static FlowLayout flow_center = new FlowLayout(FlowLayout.CENTER, 40, 20);
 
-    //Text Field
-    static JTextField inputFile = new JTextField("try.txt", 13);
-    static JTextField outputFile = new JTextField("githubout.txt", 13);
+    //Text Fields - Windows Settings
+    static JTextField inputFile = new JTextField("", 15);
+    static JTextField outputFile = new JTextField("", 15);
+    static JTextField maxChars = new JTextField("80", 5);
+    
+    //Text Fields - Mac Settings
+    //static JTextField inputFile = new JTextField("try.txt", 13);
+    //static JTextField outputFile = new JTextField("githubout.txt", 13);
+    
 
     //Labels
     static JLabel formatData = new JLabel();
-    static JLabel inputLabel = new JLabel("  Select Input File:");
+    static JLabel inputLabel = new JLabel("Select Input File:");
     static JLabel outputLabel = new JLabel("Select Output File:");
-    static JLabel justifyLabel = new JLabel("Justification");
+    static JLabel justifyLabel = new JLabel("Justification:");
+    static JLabel spacingLabel = new JLabel("Spacing:");
+    static JLabel maxCharLabel = new JLabel("How many characters would you like in the formated text file?");
 
     //Fonts
-    static Font font = new Font("name", Font.PLAIN, 34);
+    static Font font = new Font("name", Font.PLAIN, 14);
 
     //Buttons
     static JButton inputBrowseButton = new JButton("Browse");
     static JButton outputBrowseButton = new JButton("Browse");
     static JButton formatButton = new JButton("Format my File");
-    static JButton resetButton = new JButton("Reset File");
+    static JButton resetButton = new JButton("Reset");
 
     //Radio Buttons
-    static JRadioButton leftJustifyButton = new JRadioButton("Left Justified (Default)");
-    static JRadioButton rightJustifyButton = new JRadioButton("Right Justified");
+    static JRadioButton leftJustifyButton = new JRadioButton("Left (Default)");
+    static JRadioButton rightJustifyButton = new JRadioButton("Right");
+    static JRadioButton fullJustifyButton = new JRadioButton("Left/Right (Full)");
+    static JRadioButton singleSpaceButton = new JRadioButton("Single Spaced (Default)");
+    static JRadioButton doubleSpaceButton = new JRadioButton("Double Spaced");
 
     //Panels
     static JPanel mainPanel = new JPanel();
 
-    //Button Handler Class
+    /**
+     * This class implements all the actions needed when a button is pressed.
+     */
     private static class ButtonHandler implements ActionListener {
         public void actionPerformed(ActionEvent e){
             Object source = e.getSource();
@@ -81,9 +95,7 @@ public class Main {
 							input = chooser.getSelectedFile().getAbsolutePath();
 							inputFile.setText(chooser.getSelectedFile().getName());
 					}
-					catch(Exception a){
-						//Add JDialogBox for error
-					}
+					catch(Exception a){}
 
 			}
 			else if(source == outputBrowseButton)
@@ -97,9 +109,7 @@ public class Main {
 							output = chooser.getSelectedFile().getAbsolutePath();
 							outputFile.setText(chooser.getSelectedFile().getName());
 					}
-					catch(Exception a){
-						
-					}
+					catch(Exception a){}
 			}
 			else if(source == formatButton)
 			{
@@ -110,12 +120,14 @@ public class Main {
 					else{
 						textFileAnalyzer = new TextFileAnalyzer(input, output, false);
 					}
-				} catch (NullPointerException e1) {
-					//Error: input file not found
+				} 
+				catch (IOException e2) {
+					//Error: input file does not exist
                     JOptionPane.showMessageDialog(new JFrame(), "Error: Your input file doesn't exist.", "File Not Found", JOptionPane.ERROR_MESSAGE);
 				}
-				catch (IOException e2) {
-                    JOptionPane.showMessageDialog(new JFrame(), "Error: Your input file doesn't exist.", "File Not Found", JOptionPane.ERROR_MESSAGE);
+				catch (NullPointerException e1) {
+					//Error: output file does not exist
+                    JOptionPane.showMessageDialog(new JFrame(), "Error: Your output file doesn't exist.", "File Not Found", JOptionPane.ERROR_MESSAGE);
 				}
 
 				wordsProcessed = textFileAnalyzer.getWordNumber();
@@ -123,46 +135,56 @@ public class Main {
 				blanksRemoved = textFileAnalyzer.getBlankLine();
 				averageWPL = textFileAnalyzer.getAvgWordPerLine();
 				averageLL = textFileAnalyzer.getAvgCharLine();
+				//spacesAdded = textFileAnalyser.getSpacesAdded();
 
 			}
-			else if(source == resetButton)
-			{
+			else if(source == resetButton) {
 				wordsProcessed = 0;
 				lines = 0;
 				blanksRemoved = 0;
 				averageWPL = 0;
 				averageLL = 0;
-
+				spacesAdded = 0;
 			}
-
-            formatData.setText(String.format("<html><table> <tr><td align=\"right\">%s</td><td>&emsp;%d</td></tr> <tr><td align=\"right\">%s</td><td>&emsp;%d</td></tr> <tr><td align=\"right\">%s</td><td>&emsp;%d</td></tr> <tr><td align=\"right\">%s</td><td>&emsp;%.2f</td></tr> <tr><td align=\"right\">%s</td><td>&emsp;%.2f</td></tr> </table></html>", "Words Processed:", wordsProcessed, "Lines:", lines, "Blank Lines Removed:", blanksRemoved, "Average Words Per Line:", averageWPL, "Average Line Length:", averageLL));
-        }
+			
+			//Reset the text of the Data Label
+			setTextFormatDataLabel();
+		}
     }
 
-
-    public static void main(String[] args){
-    //Set Fonts for readability.
+    /**
+     * This gives all the components the correct size, font, etc. 
+     * Mostly used to make the application look prettier.
+     */
+    public static void setupComponents() {
+    	//Set Fonts for readability.
         //Buttons
         inputBrowseButton.setFont(font);
         outputBrowseButton.setFont(font);
         leftJustifyButton.setFont(font);
         rightJustifyButton.setFont(font);
+        fullJustifyButton.setFont(font);
         formatButton.setFont(font);
         resetButton.setFont(font);
+        singleSpaceButton.setFont(font);
+        doubleSpaceButton.setFont(font);
 
         //Text Fields
         inputFile.setFont(font);
         outputFile.setFont(font);
+        maxChars.setFont(font);
 
         //Labels
         inputLabel.setFont(font);
         outputLabel.setFont(font);
         justifyLabel.setFont(font);
+        spacingLabel.setFont(font);
+        maxCharLabel.setFont(font);
         formatData.setFont(font);
 
     //Set preferred button size
-        formatButton.setPreferredSize(new Dimension(1100, 60));
-        resetButton.setPreferredSize(new Dimension(1100, 60));
+        formatButton.setPreferredSize(new Dimension(500, 25));
+        resetButton.setPreferredSize(new Dimension(100, 25));
 
     //Add action listeners to buttons
         inputBrowseButton.addActionListener(new ButtonHandler());
@@ -173,82 +195,115 @@ public class Main {
         resetButton.addActionListener(new ButtonHandler());
 
     //Group Radio Buttons
-        ButtonGroup group = new ButtonGroup();
-        group.add(leftJustifyButton);
-        group.add(rightJustifyButton);
-
-    //Set Text for main data JLabel
-        /**************************************************
-         * To get the JLabel to look how I wanted, stack
-         * overflow said that i had to use HTML so I ended
-         * up making a table in HTML so that each part was
-         * on its own line and the colons would line up.
-         *
-         * Here's the HTML code in a more readable format:
-         *
-         * <html>
-         *     <table>
-         *         //Row 1
-         *         <tr>
-         *             //column 1
-         *             <td align="right">
-         *                 Words Processed:
-         *             </td>
-         *
-         *             //column 2
-         *             <td>
-         *                 &emsp;###
-         *             </td>
-         *         </tr>
-         *
-         *         //Row 2
-         *         <tr>
-         *             <td align="right">
-         *                 Lines:
-         *             </td>
-         *             <td>
-         *                 &emsp;###
-         *             </td>
-         *         </tr>
-         *
-         *         //Row 3
-         *         <tr>
-         *             <td align="right">
-         *                 Blank Lines Removed:
-         *             </td>
-         *             <td>
-         *                 &emsp;###
-         *             </td>
-         *         </tr>
-         *
-         *         //Row 4
-         *         <tr>
-         *             <td align="right">
-         *                 Average Words Per Line:
-         *             </td>
-         *             <td>
-         *                 &emsp;###.##
-         *             </td>
-         *         </tr>
-         *
-         *         //Row 5
-         *         <tr>
-         *             <td align="right">
-         *                 Average Line Length:
-         *             </td>
-         *             <td>
-         *                 &emsp;###.##
-         *             </td>
-         *         </tr>
-         *     </table>
-         * </html>
-         *
-         ************************************************/
-        formatData.setText(String.format("<html><table> <tr><td align=\"right\">%s</td><td>&emsp;%d</td></tr> <tr><td align=\"right\">%s</td><td>&emsp;%d</td></tr> <tr><td align=\"right\">%s</td><td>&emsp;%d</td></tr> <tr><td align=\"right\">%s</td><td>&emsp;%.2f</td></tr> <tr><td align=\"right\">%s</td><td>&emsp;%.2f</td></tr> </table></html>", "Words Processed:", wordsProcessed, "Lines:", lines, "Blank Lines Removed:", blanksRemoved, "Average Words Per Line:", averageWPL, "Average Line Length:", averageLL));
-
-
+        ButtonGroup justifyButtonGroup = new ButtonGroup();
+        justifyButtonGroup.add(leftJustifyButton);
+        justifyButtonGroup.add(rightJustifyButton);
+        justifyButtonGroup.add(fullJustifyButton);
+        
+        ButtonGroup spacingButtonGroup = new ButtonGroup();
+        spacingButtonGroup.add(singleSpaceButton);
+        spacingButtonGroup.add(doubleSpaceButton);
+        
+    //Set text of the important data label
+        setTextFormatDataLabel();
+    }
+    
+    /**
+     * This function is used to set the JLabel that contains the data
+     * from formating the input file. It's kind of complicated. I 
+     * read online that you have to use HTML to have multiple lines
+     * and stuff in a JLabel.
+     * 
+     * To get the JLabel to look how I wanted, stack
+     * overflow said that i had to use HTML so I ended
+     * up making a table in HTML so that each part was
+     * on its own line and the colons would line up.
+     *
+     * Here's the HTML code in a more readable format:
+     *
+     * <html>
+     *     <table>
+     *         //Row 1
+     *         <tr>
+     *             //column 1
+     *             <td align="right">
+     *                 Words Processed:
+     *             </td>
+     *
+     *             //column 2
+     *             <td>
+     *                 &emsp;###
+     *             </td>
+     *         </tr>
+     *
+     *         //Row 2
+     *         <tr>
+     *             <td align="right">
+     *                 Lines:
+     *             </td>
+     *             <td>
+     *                 &emsp;###
+     *             </td>
+     *         </tr>
+     *
+     *         //Row 3
+     *         <tr>
+     *             <td align="right">
+     *                 Blank Lines Removed:
+     *             </td>
+     *             <td>
+     *                 &emsp;###
+     *             </td>
+     *         </tr>
+     *
+     *         //Row 4
+     *         <tr>
+     *             <td align="right">
+     *                 Average Words Per Line:
+     *             </td>
+     *             <td>
+     *                 &emsp;###.##
+     *             </td>
+     *         </tr>
+     *
+     *         //Row 5
+     *         <tr>
+     *             <td align="right">
+     *                 Average Line Length:
+     *             </td>
+     *             <td>
+     *                 &emsp;###.##
+     *             </td>
+     *         </tr>
+     *         
+     *         //Row 6
+     *         <tr>
+     *             <td align="right">
+     *                 Spaces Added:
+     *             </td>
+     *             <td>
+     *                 &emsp;###
+     *             </td>
+     *         </tr>
+     *     </table>
+     * </html>
+     */
+    public static void setTextFormatDataLabel() {
+        formatData.setText(	
+        		String.format("<html> <table>") +
+        		String.format("<tr> <td align=\"right\"> %s </td><td> &emsp; %d   </td> </tr>", "Words Processed:", wordsProcessed) +
+        		String.format("<tr> <td align=\"right\"> %s </td><td> &emsp; %d   </td> </tr>", "Lines:", lines) +
+        		String.format("<tr> <td align=\"right\"> %s </td><td> &emsp; %d   </td> </tr>", "Blank Lines Removed:", blanksRemoved) +
+        		String.format("<tr> <td align=\"right\"> %s </td><td> &emsp; %.2f </td> </tr>", "Average Words Per Line:", averageWPL) +
+        		String.format("<tr> <td align=\"right\"> %s </td><td> &emsp; %.2f </td> </tr>", "Average Line Length:", averageLL) +
+        		String.format("<tr> <td align=\"right\"> %s </td><td> &emsp; %d   </td> </tr>", "Spaces Added: ", spacesAdded) +
+        		String.format("</table> </html>"));
+    }
+    
+    public static void main(String[] args){
     //Set up JPanel
-        mainPanel.setLayout(flow);
+        mainPanel.setLayout(flow_center);
+        setupComponents();
 
     //add components
         //row 1
@@ -263,18 +318,26 @@ public class Main {
         mainPanel.add(justifyLabel);
         mainPanel.add(leftJustifyButton);
         mainPanel.add(rightJustifyButton);
+        mainPanel.add(fullJustifyButton);
         //row 4
-        mainPanel.add(formatButton);
-
-        //bottom half
+        mainPanel.add(spacingLabel);
+        mainPanel.add(singleSpaceButton);
+        mainPanel.add(doubleSpaceButton);
+        //row 5
+        mainPanel.add(maxCharLabel);
+        mainPanel.add(maxChars);
+        //row 6
         mainPanel.add(formatData);
+        //row 7
+        mainPanel.add(formatButton);
+        //row 8
         mainPanel.add(resetButton);
 
     //Main Frame
         JFrame window = new JFrame("360 Project: Text Formater");
         window.setContentPane(mainPanel);
         window.pack();
-        window.setSize(1200,750);
+        window.setSize(600,570);
         window.setLocation(100,100);
         window.setVisible(true);
         window.setResizable(false);
